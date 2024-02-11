100fd9029d8d34d61abe3d5575534f225e112993
user <n.lastenko@gmail.com>
2024-02-12 00:26:58 +0600

    migrations have been made

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lms',
+        'USER': 'postgres',
+        'PASSWORD': '135790'
     }
 }
 
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
