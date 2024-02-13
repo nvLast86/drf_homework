7096f1d7b83756cb9be7829759f2cda6943a97e7
nvLast86 <n.lastenko@gmail.com>
2024-02-13 13:30:54 +0600

    lessons_count has been added to CourseSerializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,6 +3,11 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
     class Meta:
         model = Course
         fields = '__all__'
