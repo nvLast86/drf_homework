ce40383ce36d81aab9939fefb1992bf80dabce22
nvLast86 <n.lastenko@gmail.com>
2024-02-13 15:07:27 +0600

    all_lessons in CourseSerializer has been added

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,18 +2,21 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    all_lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ('id', 'name', 'preview', 'description', 'all_lessons', 'lessons_count')
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
