fed53440e534a5d52255830dffcf0d45c0b42e6f
nvLast86 <n.lastenko@gmail.com>
2024-02-18 15:03:40 +0600

    user's CRUD model has been added

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.views import UserViewSet, MyTokenObtainPairView
+
+router = DefaultRouter()
+router.register(r'user', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+              ] + router.urls
